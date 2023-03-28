639d96f7c02cabb2aac5c65c370406f59ba180d1
FabienMeunier <meunier.fabien@yahoo.fr>
2023-03-28 22:22:48 +0200

    add custom user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 
-# Create your models here.
+# class User(AbstractBaseUser): # AbstractBaseUser add to implement USERNAME_FIELD, EMAIL_FIELD, REQUIRED_FIELDS & is_active (True by default)
+#     pass
+
+class User(AbstractUser):
+    # account_number = models.fields.CharField(max_length=10, unique=True) # add any field you need
+    # email = models.EmailField(unique=True)
+    # username = None
+
+    # USERNAME_FIELD = 'email' # use email field for user name (add to be unique)
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Créateur'),
+        (SUBSCRIBER, 'Abonnés')
+    )
+    
+    profile_photo = models.ImageField(verbose_name='Photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
